9f890fce487eedd37d009d2635b35f0c9c74a6ef
Thao225 <buithienhuongthao2005@gmail.com>
2024-09-04 01:40:14 +0700

    up

--- index.js
+++ index.js
@@ -10,51 +10,92 @@ window.onload=function(){
   }
   )
   wow.init();
+  // let khoas=document.querySelectorAll(".truo")
+  // let truong=document.querySelector(".truongs .truong");
+  // let coSos=document.querySelectorAll(".truongs .truong .coSos");
+  // let nutChuyens=document.querySelectorAll(".truongs .nutchuyen li");
+  // let trai=document.getElementById("muiChuyenTrai");
+  // let phai=document.getElementById("muiChuyenPhai");
+  // let nut=0;
+  // let doDai=coSos.length-1;
+  // function taiLaiCoSo(){
+  //   let ktrTrai=coSos[nut].offsetLeft;
+  //   truong.style.right= `${-ktrTrai}px`;
+  //   let nutCuoi=document.querySelector(".truongs .nutChuyen li.nut");
+  //   nutCuoi.classList.remove('nut');
+  //   nutChuyens[nut].classList.add('nut');
+  //   clearInterval(taiLai);
+  //   taiLai= setInterval(()=>{phai.click()},5000)
+  // }
 
-let mouse=document.querySelectorAll("a")
-mouse.onmouseenter=function () {
-    this.style.cursor="pointer"
+  // phai.onclick=function(){
+  //   if(nut+1>doDai)
+  //   {
+  //     nut=0;
+  //   }
+  //   else{
+  //     nut+=1;
+  //   }
+  //   taiLaiCoSo();
+  // }
+  // trai.onclick=function(){
+  //   if(nut-1<0){
+  //     nut=doDai;
+  //   }
+  //   else{
+  //     nut=nut-1;
+  //   }
+  //   taiLaiCoSo();
+  // }
+  // let taiLai=setInterval(()=>{phai.click()},5000);
+
+  // nutChuyens.forEach((li,key)=>{
+  //   li.addEventListener('click',function(){
+  //     nut=key;
+  //     taiLaiCoSo();
+  //   })
+  // })
+
+
+
+  let truong = document.querySelector(".truong")
+    let coSos = document.querySelectorAll(".coSos")
+    let truongs = document.querySelector(".truongs")
+    var current = 0;
+    var w = truongs.offsetWidth;
+    console.log(w);
+    const changeImg = function() {
+        if (current == coSos.length-1) {
+            truong.style.transition = 'transform 0s';
+            truong.style.transform = `translateX(${0}px)`;
+            current = 0; 
+        }
+        else {
+            current++;
+            truong.style.transform = `translateX(${-w * current}px)`;
+        }
+    }
+    let clear = setInterval(() => { changeImg()}, 5000);
+    let prev = document.getElementById("muiChuyenTrai")
+    let next = document.getElementById("muiChuyenPhai")
+    console.log(prev, next)
+        prev.onclick = function() {
+            clearInterval(clear);
+            if (current == 0) {
+                current = coSos.length-1; 
+                truong.style.transition = 'transform 0s';
+                truong.style.transform = `translateX(${-w * current}px)`;
+            }
+            else {
+                current--;
+                truong.style.transform = `translateX(${-w * current}px)`;
+            }
+            clear = setInterval(() => { changeImg()}, 5000); 
+        }
+        next.onclick = function() {
+            clearInterval(clear);
+            changeImg();
+            clear = setInterval(() => { changeImg()}, 5000);
+        }
+
 }
-
-
-
-}
-
-
-
-  
-
-
-// sau 5 giây thì dừng lại
-// setTimeout(() => {
-//   clearInterval(timerId);
-// }, 5000);
-
-// video
-// var video = document.querySelector("video"),
-// container = document.querySelector("#container");
-
-// var setVideoDimensions = function() {
-// var w = video.videoWidth;
-// var videoRatio = (w / h).toFixed(2);
-
-// var containerStyles = window.getComputedStyle(container),
-//   minW = parseInt(containerStyles.getPropertyValue("width")),
-//   minH = parseInt(containerStyles.getPropertyValue("height"));
-// var widthRatio = minW / w,
-//   heightRatio = minH / h;
-// let newWidth = 0;
-// let newHeight = 0;
-// if (widthRatio > heightRatio) {
-//   newWidth = minW;
-//   newHeight = Math.ceil(newWidth / videoRatio);
-// } else {
-//   newHeight = minH;
-//   newWidth = Math.ceil(newHeight * videoRatio);
-// }
-
-// video.style.width = newWidth + "px";
-// video.style.height = newHeight + "px";
-// };
-// video.addEventListener("loadedmetadata", setVideoDimensions, false);
-// window.addEventListener("resize", setVideoDimensions, false);
--- menuVaFooter/menuVaFooter.js
+++ menuVaFooter/menuVaFooter.js
@@ -1,24 +1,28 @@
 window.onscroll = function() {
-    //---------- HIỆU ỨNG CHO THANH MENU KHI TRƯỢT TRANG -----------------------------------------------------------//
-    const menu = document.querySelector("#menu");
-    const scrollY = window.scrollY; //số pixel đã được cuộn dọc theo trục Y
-    console.log(scrollY);
-    if (scrollY > 0) {
-            menu.style.opacity = 0.85
-        } 
-        else {
-            menu.style.opacity = 1
-        }
-    // Menu with 768
-        let btn=document.querySelector("#menuRutGon>i")
-        let thanhMenu = document.querySelector("nav.mucLucs")
-        btn.onclick = function () {
-            thanhMenu.style.display="block"
-        }
-        let closemenu = document.getElementById("closeMenu")
-        closemenu.onclick = function () {
-            thanhMenu.style.display="none"
-        }
+     //---------- HIỆU ỨNG CHO THANH MENU KHI TRƯỢT TRANG -----------------------------------------------------------//
+     const menu = document.querySelector("#menu");
+     const scrollY = window.scrollY; //số pixel đã được cuộn dọc theo trục Y
+     const gioithieuKhoa=document.querySelector(".truongs")
+     const footer = document.getElementById("footer")
+     console.log(scrollY);
+     if (scrollY > 0) {
+         menu.style.position="fixed"
+         menu.style.opacity = 0.9
+         } 
+         else {
+             menu.style.position="relative"
+             menu.style.opacity = 1
+         }
+     // Menu with 768
+         let btn=document.querySelector("#menuRutGon>i")
+         let thanhMenu = document.querySelector("nav.mucLucs")
+         btn.onclick = function () {
+             thanhMenu.style.display="block"
+         }
+         let closemenu = document.getElementById("closeMenu")
+         closemenu.onclick = function () {
+             thanhMenu.style.display="none"
+         }
 
 }
 
