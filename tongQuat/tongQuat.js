c679aa43e8f5b899089484a09ef1146f3f6071fa
Thao225 <buithienhuongthao2005@gmail.com>
2024-09-04 17:41:14 +0700

    up

--- tongQuat/tongQuat.js
+++ tongQuat/tongQuat.js
@@ -1,12 +1,27 @@
 window.onscroll = function() {
     // Menu with 768
-    let btn=document.querySelector("#menuRutGon>i")
-    let thanhMenu = document.querySelector("nav.mucLucs")
+    let btn=document.querySelector("#menuRutGon>i");
+    let thanhMenu = document.querySelector("nav.mucLucs");
     btn.onclick = function () {
-        thanhMenu.style.display="block"
+        thanhMenu.style.display="block";
     }
-    let closemenu = document.getElementById("closeMenu")
+    let closemenu = document.getElementById("closeMenu");
     closemenu.onclick = function () {
-        thanhMenu.style.display="none"
+        thanhMenu.style.display="none";
+    }
+
+
+    let coMucLucs=document.querySelectorAll(".coMucLuc");
+    let thongTinTQs=document.querySelectorAll(".thongTinTQ");
+    for (let i = 0; i < coMucLucs.length; i++) {
+        coMucLucs[i].onmouseenter=function(){
+            thongTinTQs[i].style.display="flex";
+            thongTinTQs[i].addEventListener('animate__animated','animate__fadeInDown');
+        }
+        coMucLucs[i].onmouseleave=function(){
+            thongTinTQs[i].style.display="none";
+            thongTinTQs[i].addEventListener('animate__animated','animate__fadeInUp');
+        }
+        
     }
 }
